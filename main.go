bcda07884909b9daf6ea34cbba947813f1b34f28
SUT0L <SUT0L@users.noreply.github.com>
2023-07-10 22:28:23 +1000

    fucked it

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
 	"github.com/gofiber/fiber/v2/middleware/favicon"
 	"github.com/gofiber/fiber/v2/middleware/redirect"
+	"github.com/gofiber/template/html/v2"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 
+	"github.com/gofiber/session/v2"
+	"gocart/v2/auth"
 	"gocart/v2/routes"
 )
 
@@ -19,19 +26,49 @@ var (
 func main() {
 	flag.Parse()
 
+	// Initialize the database connection
+	db, err := sql.Open("sqlite3", "./db/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Wrap the database connection with sqlx for additional features
+	conn := sqlx.NewDb(db, "sqlite3")
+
+	// Read the SQL schema file
+	schemaSQL, err := ioutil.ReadFile("./db/schema.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Execute the SQL statements to create the tables
+	_, err = conn.Exec(string(schemaSQL))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new instance of the session middleware
+	sessionMiddleware := session.New(session.Config{
+		Expiration: 2 * time.Hour,
+	})
+
+	// Create a new instance of SessionManager with the database connection
+	sessionManager := &auth.SessionManager{
+		Conn: conn,
+	}
+
 	engine := html.New("./html", ".html")
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
-	// favicon, duuh
 	app.Use(favicon.New(favicon.Config{
 		File: "./favicon.ico",
-		URL: "/favicon.ico",
+		URL:  "/favicon.ico",
 	}))
 
-	// redirect to login if going to root 
 	app.Use(redirect.New(redirect.Config{
 		Rules: map[string]string{
 			"/": "/login",
@@ -39,11 +76,19 @@ func main() {
 		StatusCode: 301,
 	}))
 
-	app.Mount("/", routes.NoauthRoutes())
-	app.Mount("/dash", routes.AuthRoutes())
-	app.Mount("/api", routes.ApiRoutes())
-	
+	// Middleware that checks for a valid session on every request
+	app.Use(func(c *fiber.Ctx) error {
+		sess := sessionMiddleware.Get(c)	
+		defer sess.Save()
+		// check the shit
+		// pass to the next middleware and shit
+		return c.Next()
+		
+	})
 
+	routes.SetupNoauthRoutes(app.Group("/"), sessionManager, sessionMiddleware)
+	routes.SetupAuthRoutes(app.Group("/dash"), sessionMiddleware)
+	routes.SetupApiRoutes(app.Group("/api"))
 
 	log.Fatal(app.Listen(*port))
 }
--- routes/noauth.go
+++ routes/noauth.go
@@ -1,17 +1,97 @@
 package routes
 
 import (
+	"fmt"
+	//"time"
+
 	"github.com/gofiber/fiber/v2"
+	"gocart/v2/auth"
+	"github.com/gofiber/session/v2"
+	// "github.com/gofiber/storage/memory"
+	
 )
 
-func NoauthRoutes() *fiber.App {
-	noauth := fiber.New()
-
-	noauth.Get("/login", func(c *fiber.Ctx) error {
+func SetupNoauthRoutes(app fiber.Router, sm *auth.SessionManager, sessionMiddleware *session.Session) {
+	app.Get("/login", func(c *fiber.Ctx) error {
 		return c.Render("login", fiber.Map{
 			"title": "GoCart - Login",
 		})
 	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		type LoginRequest struct {
+			Email    string `form:"email"`
+			Password string `form:"password"`
+		}
 	
-	return noauth
+		var loginReq LoginRequest
+		if err := c.BodyParser(&loginReq); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Error on request",
+			})
+		}
+	
+		_, err := sm.Login(loginReq.Email, loginReq.Password)
+    	if err != nil {
+    	    return c.Render("login", fiber.Map{
+    	        "title":          "GoCart - Login",
+    	        "errorMessage":   "Invalid email or password",
+    	        "emailValue":     loginReq.Email,
+    	        "passwordValue":  loginReq.Password,
+    	    })
+    	}
+	
+		sess := sessionMiddleware.Get(c)
+
+		sess.Set("something", "some value")
+	 
+		if err := sess.Save(); err != nil {
+			fmt.Println(err)
+		}
+		 
+		// Go to the /dash authenticated
+		return c.Redirect("/dash")
+	})
+
+	type RegisterRequest struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Email     string `json:"email"`
+		Password  string `json:"password"`
+	}
+
+	app.Get("/register", func(c *fiber.Ctx) error {
+		return c.Render("register", fiber.Map{
+			"title": "GoCart - Register",
+		})
+	})
+
+	app.Post("/register", func(c *fiber.Ctx) error {
+		var registerReq RegisterRequest
+		err := c.BodyParser(&registerReq)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Error on request",
+			})
+		}
+
+		if registerReq.FirstName == "" || registerReq.LastName == "" || registerReq.Email == "" || registerReq.Password == "" {
+			return c.Render("register", fiber.Map{
+				"title": "GoCart - Register",
+			})
+		}
+
+		err = sm.RegisterUser(registerReq.FirstName, registerReq.LastName, registerReq.Email, registerReq.Password)
+		if err != nil {
+			return c.Render("register", fiber.Map{
+				"title":       "GoCart - Register",
+				"errorMessage": fmt.Sprintf("Error registering user. Please try again.\n%s", err),
+			})
+		}
+
+		return c.Render("register", fiber.Map{
+			"title":          "GoCart - Register",
+			"successMessage": fmt.Sprintf("User %s successfully registered", registerReq.Email),
+		})
+	})
 }
