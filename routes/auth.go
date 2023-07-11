2b64a37f1ec3f483999610cfb26bf76bdca4fb6b
SUT0L <SUT0L@users.noreply.github.com>
2023-07-11 17:06:21 +1000

    update auth route

--- routes/auth.go
+++ routes/auth.go
@@ -22,12 +22,6 @@ func SetupAuthRoutes(app fiber.Router, sessionMiddleware *session.Session) {
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-<<<<<<< HEAD
-		session_key := sessionMiddleware.Get(c)
-
-		return c.SendString(fmt.Sprintf("You're in.. Good job.. \n\n%s", session_key))
-=======
 		return c.SendString("You're in.. Good job..")
->>>>>>> 5eb5625441eb21a3673f4c7c534390b345d42511
 	})
 }
